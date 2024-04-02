fc53126ecfc1d389864b28c93fd36bb6764ba85c
Debasish Das <debasish.das83@gmail.com>
2024-04-02 08:27:42 -0700

    added product delete, create and update APIs; added an example product to play with

--- backend/base/urls/product_urls.py
+++ backend/base/urls/product_urls.py
@@ -3,5 +3,14 @@ from base.views import product_views as views
 
 urlpatterns = [
     path('', views.getProducts, name="products"),
+    path('create/', views.createProduct, name='product-create'),
     path('<str:pk>/', views.getProduct, name="product"),
+    # crud operations
+    # create a lawyer with law firm if applicable
+    # update the lawyer details
+    # delete the lawyer profile
+    # read the lawyer profile
+    
+    path('update/<str:pk>/', views.createProduct, name='product-create'),
+    path('delete/<str:pk>/', views.deleteProduct, name='product-delete'),
 ]
--- backend/base/views/product_views.py
+++ backend/base/views/product_views.py
@@ -4,6 +4,8 @@ from rest_framework.response import Response
 from base.models import Product
 from base.serializers import ProductSerializer
 from rest_framework import status
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from django.views.decorators.csrf import csrf_exempt
 
 @api_view(['GET'])
 def getProducts(request):
@@ -18,3 +20,42 @@ def getProduct(request, pk):
     product = Product.objects.get(_id=pk)
     serializer = ProductSerializer(product, many=False)
     return Response(serializer.data)
+
+@api_view(['POST'])
+@permission_classes([IsAdminUser])
+def createProduct(request):
+    user = request.user
+    product = Product.objects.create(
+        user=user,
+        name='samsung-galaxy-tab-a9-plus-5g',
+        price=269.99,
+        brand='samsung',
+        countInStock=10,
+        category='Electronics',
+        description="The new Samsung Galaxy Tab A9+ 5G for your on-the-go family fun. Fast AT&T 5G* connectivity makes every experience feel smooth and easy while the bright, engaging 11-inch screen1 and quad Dolby Atmos®-powered speakers bring entertainment to life. Plus, pre-loaded Samsung Kids app provides a safe digital environment for your little ones to happily learn and play. Give your family a great, all-in-one Tab that meets their everyday needs at a great value price!")
+    serializer = ProductSerializer(product, many=False)
+    return Response(serializer.data)
+
+@api_view(['PUT'])
+@permission_classes([IsAdminUser])
+def updateProduct(request, pk):
+    data = request.data
+    product = Product.objects.get(_id=pk)
+    product.brand = data['brand']
+    product.countInStock = data['countInStock']
+    product.description = data['description']
+    product.category = data['category']
+    product.price = data['price']
+
+    product.save()
+
+    serializer = ProductSerializer(product, many=False)
+    return Response(serializer.data)
+
+@api_view(['DELETE'])
+@csrf_exempt
+@permission_classes([IsAdminUser])
+def deleteProduct(request, pk):
+    product = Product.objects.get(_id=pk)
+    product.delete()
+    return Response('product deleted')
